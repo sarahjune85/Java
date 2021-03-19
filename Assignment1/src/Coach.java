5a2826a314054817ae46bba44cabd96ea885442e
sarahjune85 <sarahjune@gmail.com>
2021-03-19 22:25:38 +1100

    Classes split, methods building

--- Assignment1/src/Coach.java
+++ Assignment1/src/Coach.java
@@ -1,4 +1,67 @@
+import java.util.*;
 
-public class Coach {
+class Coach {
+	private String destination;
+	private double standard;
+	private double pensioner;
+	private double frequent;
+	private double totalBalance = 0;
+	private int totalSeats;
+	private char[] seats = new char[totalSeats];
+
+	public Coach(String destination, double standard, double pensioner, double frequent) {
+		this.destination = destination;
+		this.standard = standard;
+		this.pensioner = pensioner;
+		this.frequent = frequent;
+	}
+
+	public void buyStandard(int number) {
+		this.totalBalance = (standard * number) + totalBalance;
+	}
+
+	public void buyPensioner(int number) {
+		this.totalBalance = (pensioner * number) + totalBalance;
+	}
+
+	public void buyFrequent(int number) {
+		this.totalBalance = (frequent * number) + totalBalance;
+	}
+
+	// Get total bus ticket tally
+	public double getBalance() {
+		return totalBalance;
+	}
+
+	public void setPensioner(double pensioner) {
+		this.pensioner = pensioner;
+	}
+
+	// Get/Set seats:
+	public void setSeats(int seats) {
+		this.totalSeats = seats;
+	}
+
+	public void printDetails() {
+		System.out.println();
+		System.out.printf("+--------------Coach to %s-------------+\n", destination);
+		System.out.printf("%-10s%d\n", "Total seats: ", totalSeats);
+		System.out.printf("%-10s$%.2f\n", "Standard Ticket: ", standard);
+		System.out.printf("%-10s$%.2f\n", "Pensioner: ", pensioner);
+		System.out.printf("%-10s$%.2f\n", "Frequent: ", frequent);
+		System.out.println();
+	}
+
+	public void displayReceipt() {
+		System.out.println("Receipt");
+		System.out.println("*******");
+		System.out.printf("Destination: %s", destination);
+		System.out.println("Number of seats booked : x ");
+		System.out.printf("xx * Standard @ $xx.xx = $ xx.xx ");
+		System.out.printf("xx * Pensioner @ $xx.xx = $ xx.xx ");
+		System.out.printf("xx * Frequent @ $xx.xx = $ xx.xx ");
+		System.out.println(" ------ ");
+		System.out.println("Total : $xxx.xx ");
+	}
 
 }
--- Assignment1/src/StageC.java
+++ Assignment1/src/StageC.java
@@ -1,101 +1,116 @@
 import java.util.Scanner;
 
 public class StageC {
+
+	private int choice = 0;
+	private Scanner sc = new Scanner(System.in);
+	private String destination;
+	private int seatRows;
+	private int totalSeats;
+	private double standard;
+	private double pensioner;
+	private double frequent;
+	Coach coach1 = new Coach(destination, standard, pensioner, frequent);
 	
 	public StageC() {
+		startMenu();
+		coach1.setSeats(totalSeats);
+		coach1.printDetails();
+		selectionMenu();
+	}
 
-		Scanner sc = new Scanner(System.in);
-		String destination;
-		int seatRows;
-		double standard;
-		double pensioner;
-		double frequent;
-
-		System.out.println("+-----------------------------------+");
-		System.out.println("+------- DCA Bookings System -------+");
-		System.out.println("+-----------------------------------+");
-
+	public void startMenu() {
+		System.out.println("+-----------------------------------------+");
+		System.out.println("+---------- DCA Bookings System ----------+");
+		System.out.println("+-----------------------------------------+");
 		System.out.print("Enter destination: ");
 		destination = sc.nextLine();
-		// Notice: read a String using sc.nextLine()
-		// Avoids any issues with trailing newline
-
-		System.out.println("Travelling to: " + destination);
 		System.out.print("Enter # of rows of seats: ");
 		seatRows = Integer.parseInt(sc.nextLine());
-		int totalSeats = seatRows * 4;
-		// Seats should be a charArray - S, P, F - then store $ amount across the seat.
-		char[] seats = new char[totalSeats];
-				
+		totalSeats = seatRows * 4;
+		
 		System.out.print("Enter the price of a standard seat: ");
 		standard = Double.parseDouble(sc.nextLine());
-		
+
 		System.out.print("Enter the price of a pensioner seat: ");
 		pensioner = Double.parseDouble(sc.nextLine());
-		
+
 		System.out.print("Enter the price of a frequent traveller seat: ");
-		frequent = Double.parseDouble(sc.nextLine());
-		
-		sc.close();
-		// Maybe some more space here aaaa
-		System.out.println("+------- Your inputs -------+");
-		System.out.println("Total seats available on coach: " + seats);
-		System.out.printf("Standard seat price: %.2f\n", standard);
-		System.out.printf("Pensioner seat price: %.2f\n", pensioner);
-		System.out.printf("Frequent seat price: %.2f\n", frequent);
-		
-
-		
-		runMenu();
+		frequent = Double.parseDouble(sc.nextLine());		
 	}
 
+	public void selectionMenu() {
+		System.out.println("Enter a number to make a selection: ");
+		System.out.println("1. Buy a ticket");
+		System.out.println("2. Refund a ticket");
+		System.out.println("3. Display available seats");
+		System.out.println("4. Exit");
+		menu();
+	}
 
-	public void runMenu() {
-		String choice;
+	public void menu() {
 		do {
+			getUserInput();
 
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Make a selection: ");
-			choice = sc.nextLine();
 			switch (choice) {
-			case "P":
-				System.out.println("Ticket purchased");
+			case 1:
+				ticketMenu();				
 				break;
-			case "R":
-				System.out.println("Refund Issued");
+			case 2:
+				//refundMenu();
 				break;
-			case "D":
-				System.out.println("Displaying seats: ");
+			case 3:
+				//displaySeats();
+			case 4:
+				System.out.println("Farewell. Happy travelling!");
+				System.exit(0);
 				break;
-			case "X":
-				System.out.println("BYEEEEE");
-				break;
-			// default is a catch-all, is optional.
 			default:
-				System.out.println("Invalid input, try again doofus.");
-
+				break;
 			}
-		} while (!choice.equals("X"));
-
-	} 
-	
-	public void displayReceipt() {
-	/*	
-		Receipt 
-		******* 
-		Destination : xxxxxxxxxxxxx
-		Number of seats booked : x 
-		 xx * Standard @ $xx.xx = $ xx.xx 
-		 xx * Pensioner @ $xx.xx = $ xx.xx 
-		 xx * Frequent @ $xx.xx = $ xx.xx 
-		 ------ 
-		 Total : $xxx.xx 
-		 
-		*/
+		} while (choice != 0);
 	}
-	
+
+	public void ticketMenu() {
+		System.out.println("1. Standard ticket");
+		System.out.println("2. Pensioner ticket");
+		System.out.println("3. Frequent ticket");
+		System.out.println("4. Back to main menu");
+		getUserInput();
+		switch (choice) {
+		case 1:			
+			//Standard Ticket
+			System.out.println("# of Standard tickets to purchase: ");
+			getUserInput();
+			coach1.displayReceipt();
+			break;
+		case 2:
+			//Pensioner ticket
+			System.out.println("# of Pensioner tickets to purchase: ");
+			getUserInput();
+			break;			
+		case 3:
+			//Frequent Ticket
+			System.out.println("# of Frequent tickets to purchase: ");
+			getUserInput();
+			break;
+		case 4:
+			System.out.println("Returning to main menu...");
+			selectionMenu();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private int getUserInput() throws NumberFormatException {
+		Scanner in = new Scanner(System.in);
+		choice = Integer.parseInt(in.nextLine());
+		return choice;
+	}
+
+
 	public static void main(String[] args) {
 		StageC app = new StageC();
 	}
-
 }
